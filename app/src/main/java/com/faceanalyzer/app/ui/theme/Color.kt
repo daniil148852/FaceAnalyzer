0000d00a6b5be4be86ba988c17def427218d396b
daniil148852 <daniildan850@gmail.com>
2026-02-01 11:56:30 +0100

    Create Color.kt

---
+++ app/src/main/java/com/faceanalyzer/app/ui/theme/Color.kt
@@ -0,0 +1,37 @@
+package com.faceanalyzer.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom App Colors
+val GradientStart = Color(0xFF667EEA)
+val GradientEnd = Color(0xFF764BA2)
+
+val HappyColor = Color(0xFF4CAF50)
+val SadColor = Color(0xFF2196F3)
+val AngryColor = Color(0xFFF44336)
+val SurprisedColor = Color(0xFFFF9800)
+val NeutralColor = Color(0xFF9E9E9E)
+val FearColor = Color(0xFF9C27B0)
+val DisgustColor = Color(0xFF795548)
+
+val CardBackground = Color(0xFF1E1E2E)
+val CardBackgroundLight = Color(0xFFF5F5F5)
+val SurfaceDark = Color(0xFF121218)
+val SurfaceLight = Color(0xFFFFFFFF)
+
+val MeshColor = Color(0xFF00FF88)
+val MeshColorSecondary = Color(0xFF00BFFF)
+
+val HealthExcellent = Color(0xFF00E676)
+val HealthGood = Color(0xFF69F0AE)
+val HealthFair = Color(0xFFFFD54F)
+val HealthPoor = Color(0xFFFF5252)
