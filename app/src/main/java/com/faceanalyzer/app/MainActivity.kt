8f9a8389c337b824bc668aa1429dbad393dfaac9
daniil148852 <daniildan850@gmail.com>
2026-02-01 12:04:04 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/faceanalyzer/app/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.faceanalyzer.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.faceanalyzer.app.ui.screens.MainScreen
+import com.faceanalyzer.app.ui.theme.FaceAnalyzerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        
+        setContent {
+            FaceAnalyzerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
